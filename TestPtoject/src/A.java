1e974e1e6762695116c8a8009a9cdaab6e4ce64b
Ramkumar <Ramkumar@DESKTOP-ON6UF7E>
2025-07-09 14:11:01 +0530

    Maven and Hello Statement

--- TestPtoject/src/A.java
+++ TestPtoject/src/A.java
@@ -3,7 +3,8 @@ public class A {
 
 	public static void main(String[] args) 
 	{		
- System.out.println("========== Git ==========");
+            System.out.println("========== Git ==========");
+            System.out.println(" Hello boss");
 	}
 
 }
